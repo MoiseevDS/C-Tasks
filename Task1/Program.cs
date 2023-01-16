9a84a9a8517c9846152ea9ef6b36194a126d9d2b
Dmitriy <dmitiry_moiseev@inbox.ru>
2023-01-17 00:55:35 +0300

    add Task 1 is completed

---
+++ Task1/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.Clear();
+Console.Write("Введдите 1-ю цифру: ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введдите 1-ю цифру: ");
+int m = Convert.ToInt32(Console.ReadLine());
+if (n > m)
+    Console.WriteLine(n);
+else if (n < m)
+    Console.WriteLine(m);
