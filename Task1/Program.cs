17657507a7fd5f14b32afcc67ad62eef95ea42d1
Dmitriy <dmitiry_moiseev@inbox.ru>
2023-01-17 01:43:56 +0300

    add Task 4 is completed

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -2,7 +2,7 @@
 Console.Clear();
 Console.Write("Введдите 1-ю цифру: ");
 int n = Convert.ToInt32(Console.ReadLine());
-Console.Write("Введдите 1-ю цифру: ");
+Console.Write("Введдите 2-ю цифру: ");
 int m = Convert.ToInt32(Console.ReadLine());
 if (n > m)
     Console.WriteLine(n);
---
+++ Task2/Program.cs
@@ -0,0 +1,17 @@
+﻿//Максимальное число из 3х
+Console.Clear();
+Console.Write("Введдите 1-ю цифру: ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введдите 2-ю цифру: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введдите 3-ю цифру: ");
+int a = Convert.ToInt32(Console.ReadLine());
+int max = 0;
+if (n >= max)
+    max = n;
+if (m >= max)
+    max = m;
+if (a >= max)
+    max = a;
+Console.Write("Наибольшее число: ");
+Console.WriteLine(max);
