a06a15997ef420ec7e15baba02c9babd157ec471
Dmitriy <dmitiry_moiseev@inbox.ru>
2023-01-17 02:19:02 +0300

    add Task 4 is completed

---
+++ Task4/Program.cs
@@ -0,0 +1,7 @@
+﻿// Все чётные числа от 1 до N
+Console.Clear();
+Console.Write("Веедите число :");
+int n = Convert.ToInt32(Console.ReadLine());
+for (int i = 1; i <= n; i++)
+    if (i % 2 == 0)
+        Console.WriteLine(i);
