c2921cf3c49845baf5424923e8d89fc7b5587e90
Dmitriy <dmitiry_moiseev@inbox.ru>
2023-01-17 15:08:38 +0300

    add Task 5 Task10 is completed

---
+++ Task5/Program.cs
@@ -0,0 +1,7 @@
+﻿//Задача 10 Вторая цифра 3х значного чисала
+Console.Clear();
+Console.Write("Введите 3-х значное число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int n1 = n / 10;
+int n3 = n1 % 10;
+Console.WriteLine(n3);
