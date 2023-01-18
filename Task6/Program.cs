134006f3a2a747c22afa2b6595f17fd89a09126e
Dmitriy <dmitiry_moiseev@inbox.ru>
2023-01-18 22:50:48 +0300

    add Task 7 and completed Task 15

--- Task6/Program.cs
+++ Task6/Program.cs
@@ -2,11 +2,9 @@
 Console.Clear();
 Console.Write("Введите число: ");
 int n = Convert.ToInt32(Console.ReadLine());
-int resalt = 0;
 
 while (n / 1000 != 0)
 {
-    resalt = n % 10;
     n = n / 10;
 }
 if (n / 100 == 0)
---
+++ Task7/Program.cs
@@ -0,0 +1,9 @@
+﻿// Задача 15 является ли день выходным 
+Console.Clear();
+Console.Write("Введите день недели: ");
+int day = Convert.ToInt32(Console.ReadLine());
+if (day == 7 || day == 6)
+    Console.WriteLine("Выходной день!!!");
+else
+    Console.WriteLine("Будний день");
+
