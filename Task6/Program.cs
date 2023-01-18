1dc8b3706fd8fdd5cd70634ce0d810398c6b7394
Dmitriy <dmitiry_moiseev@inbox.ru>
2023-01-18 22:19:34 +0300

    add Task 6  is completed Task 13

---
+++ Task6/Program.cs
@@ -0,0 +1,16 @@
+﻿//Задача 13 Третья цифра многозначного чисала
+Console.Clear();
+Console.Write("Введите число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int resalt = 0;
+
+while (n / 1000 != 0)
+{
+    resalt = n % 10;
+    n = n / 10;
+}
+if (n / 100 == 0)
+    Console.WriteLine("Нет 3-й цифры");
+else
+    Console.WriteLine($"Третяя цифра: {n % 10}");
+
