904ddfe4cf86cba86051328764ed8320cd909b7b
Dmitriy <dmitiry_moiseev@inbox.ru>
2023-01-19 15:14:00 +0300

    add Task 8 and completed additional task

---
+++ Task8/Program.cs
@@ -0,0 +1,16 @@
+﻿// Дополнительная задача "Второе максимальное значение"
+Console.Clear();
+int n = Convert.ToInt32(Console.ReadLine());
+int max = 0, max2 = 0;
+while (n != 0)
+{
+    n = Convert.ToInt32(Console.ReadLine());
+    if (n > max)
+    {
+        max2 = max;
+        max = n;
+    }
+    else if (n > max2 && n != max)
+        max2 = n;
+}
+Console.WriteLine(max2);
