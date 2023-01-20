498142f00aedb69e56a10f008d7961eb7245680f
Dmitriy <dmitiry_moiseev@inbox.ru>
2023-01-20 15:35:34 +0300

    added Math class

--- Task9/Program.cs
+++ Task9/Program.cs
@@ -1,12 +1,12 @@
 ﻿// Task 19 Полиндром
 Console.Clear();
 Console.Write("Введите 5ти значное число: ");
-int n = Convert.ToInt32(Console.ReadLine());
-int revers = n % 10 * 10000 + (n / 10) % 10 * 1000 + (n / 100) % 10 * 100 + (n / 1000) % 10 * 10 + n / 10000; ;
+int n = Math.Abs(Convert.ToInt32(Console.ReadLine()));
+int revers = n % 10 * 10000 + (n / 10) % 10 * 1000 + (n / 100) % 10 * 100 + (n / 1000) % 10 * 10 + n / 10000;
 while (n <= 9999 || n >= 1000000)
 {
     Console.WriteLine("Не допустимое число!\nВведите 5ти значное число: ");
-    n = Convert.ToInt32(Console.ReadLine());
+    n = Math.Abs(Convert.ToInt32(Console.ReadLine()));
 }
 
 if (n == revers)
