37cc1761560bd7cec072af03718aadb95b704f17
Dmitriy <dmitiry_moiseev@inbox.ru>
2023-01-27 01:14:45 +0300

    add Task 15 and completed Task 29

---
+++ Task15/Program.cs
@@ -0,0 +1,12 @@
+﻿// Task 29
+
+Console.Clear();
+Console.Write("Введите размер массива: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int[] array = new int[n];
+for (int i = 0; i < array.Length; i++)
+{
+    Console.Write("Введите число: ");
+    array[i] = Convert.ToInt32(Console.ReadLine());
+}
+Console.WriteLine($"Содержимое массива: [{string.Join(", ", array)}]");
