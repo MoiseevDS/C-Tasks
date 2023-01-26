1e923ca48e2ad61da79e7ddc3079a3b77b90b2d6
Dmitriy <dmitiry_moiseev@inbox.ru>
2023-01-27 00:37:34 +0300

    add Task 13 and completed Task 25

---
+++ Task13/Program.cs
@@ -0,0 +1,14 @@
+﻿// Задача 25
+
+Console.Clear();
+Console.Write("Введите число: ");
+int b = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите степень: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int degree = b;
+
+for (int i = 1; i < n; i++)
+    degree = degree * b;
+
+Console.WriteLine(degree);
+
