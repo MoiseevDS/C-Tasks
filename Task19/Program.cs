c2e71591440a5bb1c754bcb743ecb82b11263616
Dmitriy <dmitiry_moiseev@inbox.ru>
2023-01-31 01:05:06 +0300

    add Task 19 decision Task 36

---
+++ Task19/Program.cs
@@ -0,0 +1,21 @@
+﻿// Task 36
+
+void InputArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+        array[i] = new Random().Next(-100, 101);
+}
+
+
+Console.Clear();
+Console.Write("Введите кол-во элементов массива: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int[] array = new int[n];
+InputArray(array);
+Console.WriteLine($"Начальный массив: [{string.Join(", ", array)}]");
+int sumNotEven = 0;
+
+for (int i = 1; i < array.Length; i = i + 2)
+    sumNotEven += array[i];
+
+Console.WriteLine($"Сумма чисел нечетных элементов: {sumNotEven}");
