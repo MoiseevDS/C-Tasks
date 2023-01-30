3f444398b3140d641adb8b017ec06e3a72560d08
Dmitriy <dmitiry_moiseev@inbox.ru>
2023-01-31 00:26:53 +0300

    add Task 18 decision Task 34

---
+++ Task18/Program.cs
@@ -0,0 +1,24 @@
+﻿// Task 34
+
+void InputArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+        array[i] = new Random().Next(100, 1000);
+}
+
+
+Console.Clear();
+Console.Write("Введите кол-во элементов массива: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int[] array = new int[n];
+InputArray(array);
+Console.WriteLine($"Начальный массив: [{string.Join(", ", array)}]");
+int count = 0;
+
+for (int i = 0; i < array.Length; i++)
+{
+    if (array[i] % 2 == 0)
+        count = count + 1;
+}
+
+Console.WriteLine($"Четных числе в массиве: {count}");
