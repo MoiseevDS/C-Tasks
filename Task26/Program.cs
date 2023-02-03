e2943a428b72be0345494b4ed3eed4328318f266
Dmitriy <dmitiry_moiseev@inbox.ru>
2023-02-03 12:02:24 +0300

    add Task 26 and completed Task 47

---
+++ Task26/Program.cs
@@ -0,0 +1,22 @@
+﻿// Task 47 
+
+
+void InputMatrix(double[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = Convert.ToDouble(new Random().Next(-100, 100)) / 10; // [1, 20]
+            Console.Write($"{matrix[i, j]} \t");
+        }
+        Console.WriteLine();
+    }
+}
+
+
+Console.Clear();
+Console.Write("Введите размеры массива: ");
+int[] size = Console.ReadLine().Split().Select(x => int.Parse(x)).ToArray();
+double[,] matrix = new double[size[0], size[1]];
+InputMatrix(matrix);
