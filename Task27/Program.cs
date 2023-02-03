01324233dafbf49bb05d45352ab360e3d03d99a1
Dmitriy <dmitiry_moiseev@inbox.ru>
2023-02-03 12:30:54 +0300

    add Task 27 and completed Task 50

---
+++ Task27/Program.cs
@@ -0,0 +1,36 @@
+﻿// Task 50 
+
+void InputMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = new Random().Next(1, 10); // [1, 9]
+            Console.Write($"{matrix[i, j]} \t");
+        }
+        Console.WriteLine();
+    }
+}
+
+
+Console.Clear();
+Console.Write("Введите размеры массива: ");
+int[] size = Console.ReadLine().Split().Select(x => int.Parse(x)).ToArray();
+int[,] matrix = new int[size[0], size[1]];
+InputMatrix(matrix);
+
+Console.Write("Введите строку: ");
+int pos1 = Convert.ToInt32(Console.ReadLine()) - 1;
+Console.Write("Введите столбец: ");
+int pos2 = Convert.ToInt32(Console.ReadLine()) - 1;
+
+
+if (pos1 < 0 || pos1 > matrix.GetLength(0) - 1 || pos2 < 0 || pos2 > matrix.GetLength(1) - 1)
+{
+    Console.WriteLine("Элемент не существует  ");
+}
+else
+{
+    Console.WriteLine("Значение элемента массива = {0}", matrix[pos1, pos2]);
+}
