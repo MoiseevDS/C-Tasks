3eca5bbb3854a39c0c5040afc387f618722cc0b0
Dmitriy <dmitiry_moiseev@inbox.ru>
2023-02-03 10:20:15 +0300

    add Task 22 and completed Task 41

--- Task22/Program.cs
+++ Task22/Program.cs
@@ -14,3 +14,22 @@ while (n != 0)
     }
 }
 Console.WriteLine($"Колличество чисел больше 0 = {cont}");
+
+
+
+
+
+// Через цикл
+/*
+Console.Clear();
+Console.Write("Введите число: ");
+int n = Convert.ToInt32(Console.ReadLine()), count = 0;
+for (int i = 0; i < n; i++)
+{
+    Console.Write("Введите число: ");
+    int x = Convert.ToInt32(Console.ReadLine());
+    if (x > 0)
+        count++; // count = count + 1;
+}
+Console.WriteLine($"Результат: {count}");
+*/
