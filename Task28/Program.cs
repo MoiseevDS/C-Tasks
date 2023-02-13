cbdb9380d99dec4cdcb97192f5d84a28ad7e8faf
Dmitriy <dmitiry_moiseev@inbox.ru>
2023-02-13 15:27:16 +0300

    add Task28 and completed Task 52

---
+++ Task28/Program.cs
@@ -0,0 +1,36 @@
+﻿// Task 52
+
+
+
+
+void InputMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = new Random().Next(1, 10); // [1, 9]
+            Console.Write($"{matrix[i, j]} \t");
+        }
+        Console.WriteLine();
+    }
+}
+
+Console.Clear();
+Console.Write("Введите размеры массива: ");
+int[] size = Console.ReadLine().Split().Select(x => int.Parse(x)).ToArray();
+int[,] matrix = new int[size[0], size[1]];
+InputMatrix(matrix);
+
+Console.WriteLine("---------------------------");
+Console.WriteLine(matrix.GetLength(0));
+for (int j = 0; j < matrix.GetLength(1); j++)
+{
+    double sum = 0;
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        sum += matrix[i, j];
+    }
+    Console.Write($"{sum / matrix.GetLength(0)} ");
+}
+Console.ReadLine();
