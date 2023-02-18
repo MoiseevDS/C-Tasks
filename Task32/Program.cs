b085407d8aeb62ff578a9952c30135352590e3bb
Dmitriy <dmitiry_moiseev@inbox.ru>
2023-02-19 01:06:33 +0300

    add Task32 and completed Task 60

---
+++ Task32/Program.cs
@@ -0,0 +1,41 @@
+﻿// Task60
+
+
+int[,,] matrix3D = new int[2, 2, 2];
+// Функция вывода индекса элементов 3D массива
+void PrintMatrix(int[,,] array)
+{
+    for (int i = 0; i < matrix3D.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix3D.GetLength(1); j++)
+        {
+            Console.WriteLine();
+            for (int k = 0; k < matrix3D.GetLength(2); k++)
+            {
+                Console.Write($"{matrix3D[i, j, k]}({i},{j},{k}) ");
+            }
+        }
+    }
+}
+
+// Функция заполнения 3D массива не повторяющимеся числами
+void FillMatrix(int[,,] array)
+{
+    int count = 10;
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            for (int k = 0; k < array.GetLength(2); k++)
+            {
+                array[k, i, j] += count;
+                count += 3;
+            }
+        }
+    }
+}
+
+
+
+FillMatrix(matrix3D);
+PrintMatrix(matrix3D);
