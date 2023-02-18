7d732cae74ba3a65036bbc77b0c5069e71a37e42
Dmitriy <dmitiry_moiseev@inbox.ru>
2023-02-19 01:33:30 +0300

    add Task34 and completed Task 66

---
+++ Task34/Program.cs
@@ -0,0 +1,27 @@
+﻿// Task 66
+
+int SumMN(int m, int n)
+{
+    int res = m;
+    if (m == n)
+        return 0;
+    else
+    {
+        m++;
+        res = m + SumMN(m, n);
+        return res;
+    }
+}
+
+void SumOtMDoN(int m, int n)
+{
+    Console.Write(SumMN(m - 1, n));
+}
+
+Console.Write("Введите число M: ");
+int m = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите число N: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+SumOtMDoN(m, n);
